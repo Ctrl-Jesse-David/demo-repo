d9d13074bcd60a7cbd537d0c63766ad6e2f33a70
Jesse David Francisco <francisco.jessedavid@gmail.com>
2025-03-21 13:57:05 +0800

    new files and new wordscapes

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Watch tutorial on youtube!
 ## Local Development
 
 1. Open ondex.html in your browser
+
+    # def shuffle_letters(self):
+    #     self.level_letters = "".join(random.sample(self.level_letters, len(self.level_letters)))
+    
--- wordscapes.py
+++ wordscapes.py
@@ -1,100 +1,80 @@
-
 import os
 import random
+from game_levels import level_letters, valid_words, grid, positions
 
-'''
-there should be an interface
-'''
-
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def display_grid(grid):
-    clear_screen()
-    print("\nWordscapes - Terminal Edition\n")
-    for row in grid:
-        print(" ".join(row))
-    print("\n")
-
-def shuffle_letters(letters):
-    return "".join(random.sample(letters, len(letters)))
-
-def generate_grid():
-    return [
-        ["-", "-", "-", "x", "-"],
-        ["-", "-", "-", "x", "-"],
-        ["-", "x", "x", "x", "x"],
-        ["-", "x", "-", "-", "-"],
-        ["-", "x", "-", "-", "-"],
-        ["x", "x", "x", "x", "-"],
-        ["x", "-", "-", "-", "-"],
-        ["x", "-", "-", "-", "-"],
-        ["x", "-", "-", "-", "-"]
-    ]
-
-# shpuld make a generate grid that fetches grids, then returns the grid,
-# instead of it being constant, it should be reusable
-# and grid should be imported from another file
-
-def update_grid(grid, word):
-    positions = {
-        "TEAM": [(2, 1), (2, 2), (2, 3), (2, 4)],
-        "MEAT": [(5, 0), (5, 1), (5, 2), (5, 3)],
-        "MATE": [(5, 0), (6, 0), (7, 0), (8, 0)],
-        "TEA": [(0, 3), (1, 3), (2, 3)],
-        "TAME": [(2, 1), (3, 1), (4, 1), (5, 1)]
-    }
+class GameGrid:
+    def __init__(self, grid, positions):
+        self.grid = grid
+        self.positions = positions
     
-    if word in positions:
-        for idx, (row, col) in enumerate(positions[word]):
-            grid[row][col] = word[idx]
-        return True
-    return False
+    def update_grid(self, word):
+        if word in self.positions:
+            for idx, (row, col) in enumerate(self.positions[word]):
+                self.grid[row][col] = word[idx]
+            return True
+        return False
 
-def is_valid_guess(guess, level_letters):
-    return all(guess.count(letter) <= level_letters.count(letter) for letter in guess)
-
-# Same goes with this function UpdateGrid
-# It should allow to get positions
-# which are dictionaries
-
-def wordscapes():
-    level_letters = "TEAM"
-    valid_words = {"TEAM", "MEAT", "MATE", "TEA", "TAME"}
-    found_words = set()
-    grid = generate_grid()
+    def refresh_display(self):
+        return os.system('cls' if os.name == 'nt' else 'clear')
     
-    while found_words != valid_words:
-        display_grid(grid)
-        print(f"Available letters: {level_letters}")
-        print("Commands: [shuffle] to shuffle letters, [exit] to quit\n")
-        
-        guess = input("Enter a word: ").upper()
-        
-        if guess == "SHUFFLE":
-            level_letters = shuffle_letters(level_letters)
-            continue
-        elif guess == "EXIT":
-            break
-        elif guess in valid_words:
-            if guess in found_words:
-                print("Word already found!")
-            elif is_valid_guess(guess, level_letters):
-                found_words.add(guess)
-                update_grid(grid, guess)
-                print("Correct!")
+    def display(self):
+        self.refresh_display()
+        print("\nWordscapes\n")
+        for row in self.grid:
+            print(" ".join(row))
+        print("\n")
+
+
+
+class WordscapesGame:
+    def __init__(self, level_letters, valid_words, grid, positions):
+        self.level_letters = level_letters
+        self.valid_words = valid_words
+        self.found_words = set()
+        self.grid = GameGrid(grid, positions)
+    
+
+    def shuffle_letters(self):
+        pass
+    
+    def is_valid_guess(self, guess):
+        return all(guess.count(letter) <= self.level_letters.count(letter) for letter in guess)
+    
+    def play(self):
+        while self.found_words != self.valid_words:
+            self.grid.display()
+            print(f"Available letters: {self.level_letters}")
+            print("Commands: [shuffle] to shuffle letters, [exit] to quit\n")
+            
+            guess = input("Enter a word: ").upper()
+            
+            if guess == "SHUFFLE":
+                self.shuffle_letters()
+                continue
+
+            elif guess == "EXIT":
+                # than k u for playing
+                break
+
+            elif guess in self.valid_words:
+                if guess in self.found_words:
+                    print("Word already found!")
+
+                elif self.is_valid_guess(guess):
+                    self.found_words.add(guess)
+                    self.grid.update_grid(guess)
+                    print("Correct!")
+    
+                else:
+                    print("Invalid word.")
             else:
-                print("Invalid word. Check your letters!")
-        else:
-            print("Word not in list.")
+                print("Word not in list.")
+            
+            input("Press Enter to continue...")
         
-        input("Press Enter to continue...")
-        '''
-        should be timed
-        '''
-    
-    display_grid(grid)
-    print("Congratulations! You completed the level.")
+        self.grid.display()
+        print("Congratulations! You guessed all the words.")
 
 if __name__ == "__main__":
-    wordscapes()
+    game = WordscapesGame(level_letters, valid_words, grid, positions)
+    game.play()
